55cd471186f172d5e56f93773a29cfe7f78defde
mhlee1412 <40865188+mhlee1412@users.noreply.github.com>
2025-10-16 14:08:33 +0900

    CT 파이프라인 완료

--- pipelines/continuous_training/continuous_training_dag.py
+++ pipelines/continuous_training/continuous_training_dag.py
@@ -53,7 +53,7 @@ with DAG(
     data_preprocessing = BashOperator(
         task_id="data_preprocessing",
         bash_command=f"cd {airflow_dags_path}/pipelines/continuous_training/docker &&"
-        "dokcer compose up --build && docker compose down",
+        "docker compose up --build && docker compose down",
         env={
 		"PYTHON_FILE": "/home/codespace/data_preprocessing/preprocessor.py",
 		"MODEL_NAME": "credit_score_classification",
@@ -63,6 +63,17 @@ with DAG(
 	    retries=1,
     )
 
-    training = EmptyOperator(task_id="model_training")
+    training = BashOperator(
+        task_id="model_training",
+        bash_command=f"cd {airflow_dags_path}/pipelines/continuous_training/docker &&"
+	    "docker compose up --build && docker compose down",
+        env={
+		"PYTHON_FILE": "/home/codespace/training/trainer.py",
+		"MODEL_NAME": "credit_score_classification",
+		"BASE_DT": kst_ds_template,
+        },
+        append_env=True,
+	    retries=1,
+    )
 
     data_extract >> data_preprocessing >> training
